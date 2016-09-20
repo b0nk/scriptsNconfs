829663692e425e18d4ca888c8e89bd37cdae6979
b0nk <fsantos.pt@hotmail.com>
2016-09-20 01:15:05 +0100

    updated mp4_CFR.bat

--- mp4_CFR.bat
+++ mp4_CFR.bat
@@ -6,6 +6,7 @@ SET ASTREAM=1
 SET AFILE=a.ac3
 
 for %%i in (*.%FILETYPE%) do (
+  title "Converting %%i"
   ffmpeg -i "%%i" -c:v copy -map_metadata -1 -bsf:v h264_mp4toannexb raw.264 ^
          -map 0:%ASTREAM% -c:a ac3 -b:a 640k -map_metadata -1 %AFILE% ^
          -map 0:5 -c:s copy "%%~ni.srt" -y

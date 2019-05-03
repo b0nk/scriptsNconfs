f771ce52051bdf98b83475196c83cd58ac1ee8ac
b0nk <fsantos.pt@hotmail.com>
2019-05-03 12:22:09 +0100

    support for HEVC

--- mux.sh
+++ mux.sh
@@ -10,16 +10,23 @@ VSETTINGS="-c:v copy -map_metadata -1 -bsf:v h264_mp4toannexb"
 rm ./*_isotmp 2>/dev/null ;
 
 for i in *.$FILETYPE ; do
+  VCODEC=$(mediainfo --Output=Video\;%Format% "$i");
   FRAMERATE=$(mediainfo --Output=Video\;%FrameRate% "$i");
   ACODEC=$(mediainfo --Output=Audio\;%Format%\\n "$i" | sed -n "$ASTREAM"p);
   FILENAME=$(basename "$i" .$FILETYPE);
 
+  if [ $VCODEC = "HEVC" ] ;
+  then
+    VSETTINGS="-c:v copy -map_metadata -1 -bsf:v hevc_mp4toannexb"
+    VFILE=0v.265
+  fi
+
   ASETTINGS="-map 0:$ASTREAM -c:a copy -map_metadata -1"
 
-  if [ "$ACODEC" = "AAC" ] ;
+  if [ $ACODEC = "AAC" ] ;
   then
     AFILE=0a.aac
-  elif [ "$ACODEC" != "AC-3" ] ;
+  elif [ $ACODEC != "AC-3" ] ;
   then
     ASETTINGS="-map 0:$ASTREAM -c:a ac3 -b:a 640k -map_metadata -1"
   fi

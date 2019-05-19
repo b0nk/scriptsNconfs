a95aa1c1551fe6f83e313f9e5cf1223b3a59df2e
b0nk <fsantos.pt@hotmail.com>
2019-05-19 20:06:04 +0100

    shellcheck fix

--- mux.sh
+++ mux.sh
@@ -9,13 +9,13 @@ VSETTINGS="-c:v copy -map_metadata -1 -bsf:v h264_mp4toannexb"
 
 rm ./*_isotmp 2>/dev/null ;
 
-for i in *.$FILETYPE ; do
+for i in *."$FILETYPE" ; do
   VCODEC=$(mediainfo --Output=Video\;%Format% "$i");
   FRAMERATE=$(mediainfo --Output=Video\;%FrameRate% "$i");
   ACODEC=$(mediainfo --Output=Audio\;%Format%\\n "$i" | sed -n "$ASTREAM"p);
   FILENAME=$(basename "$i" .$FILETYPE);
 
-  if [ $VCODEC = "HEVC" ] ;
+  if [ "$VCODEC" = "HEVC" ] ;
   then
     VSETTINGS="-c:v copy -map_metadata -1 -bsf:v hevc_mp4toannexb"
     VFILE=0v.265
@@ -23,20 +23,20 @@ for i in *.$FILETYPE ; do
 
   ASETTINGS="-map 0:$ASTREAM -c:a copy -map_metadata -1"
 
-  if [ $ACODEC = "AAC" ] ;
+  if [ "$ACODEC" = "AAC" ] ;
   then
     AFILE=0a.aac
-  elif [ $ACODEC != "AC-3" ] ;
+  elif [ "$ACODEC" != "AC-3" ] ;
   then
     ASETTINGS="-map 0:$ASTREAM -c:a ac3 -b:a 640k -map_metadata -1"
   fi
 
-  if [ $SSTREAM -ne 0 ] ;
+  if [ "$SSTREAM" -ne 0 ] ;
   then
     SUBS="-map 0:$SSTREAM -c:s copy $FILENAME.srt"
   fi
 
-  ffmpeg -i "$i" $VSETTINGS $VFILE $ASETTINGS $AFILE $SUBS -y -hide_banner ;
+  ffmpeg -i "$i" "$VSETTINGS" "$VFILE" "$ASETTINGS" "$AFILE" "$SUBS" -y -hide_banner ;
   rc=$?
 
   if [ $rc -eq 0 ] ;

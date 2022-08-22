b2f2894c8012b1d7c2e331cda47acbe752b0385f
b0nk <fsantos.pt@hotmail.com>
2022-08-22 20:35:34 +0100

    use a random filename so I can run this script on other directories at the same time

--- mux.sh
+++ mux.sh
@@ -9,6 +9,7 @@ AFILE=0a.ac3
 VFILE=0v.264
 ASETTINGS="-map 0:$ASTREAM -c:a copy -map_metadata -1"
 VSETTINGS="-c:v copy -map_metadata -1 -bsf:v h264_mp4toannexb"
+WORKINGDIR=$HOME/.tmp/vega-mux
 
 [ -z "$1" ] || [ "0" = "$1" ] && ASTREAMI=1 ;
 
@@ -18,12 +19,15 @@ for i in *."$FILETYPE" ; do
   ACODEC=$(mediainfo --Output=Audio\;%Format%\\n "$i" | sed -n "$ASTREAMI"p);
   ABITRATE=$(mediainfo --Output=Audio\;%BitRate%\\n "$i" | sed -n "$ASTREAMI"p);
   FILENAME=$(basename "$i" .$FILETYPE);
+  RND=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8); # 8 random chars
+  AFILE=0a-$RND.ac3
+  VFILE=0v-$RND.264
 
-  [ "$VCODEC" = "HEVC" ] && VSETTINGS="-c:v copy -map_metadata -1 -bsf:v hevc_mp4toannexb" && VFILE=0v.265
+  [ "$VCODEC" = "HEVC" ] && VSETTINGS="-c:v copy -map_metadata -1 -bsf:v hevc_mp4toannexb" && VFILE=0v-$RND.265
 
   if [ "$ACODEC" = "AAC" ] ;
   then
-    AFILE=0a.aac
+    AFILE=0a-$RND.aac
   elif [ "$ACODEC" = "E-AC-3" ] ;
   then
     ASETTINGS="-map 0:$ASTREAM -c:a ac3 -b:a $ABITRATE -map_metadata -1"
@@ -34,29 +38,28 @@ for i in *."$FILETYPE" ; do
 
   [ -n "$SSTREAM" ] && SUBS="-map 0:$SSTREAM -c:s copy $FILENAME.srt"
 
-  ffmpeg -i "$i" $VSETTINGS $VFILE $ASETTINGS $AFILE $SUBS -y -hide_banner ;
+  ffmpeg -i $i $VSETTINGS "$WORKINGDIR"/$VFILE $ASETTINGS "$WORKINGDIR"/$AFILE $SUBS -y -hide_banner ;
   rc=$?
 
   if [ $rc -ne 0 ] ;
   then
     echo ERROR on ffmpeg : "$i" ;
-    break
+    exit 1
   elif [ $DELETE -eq 1 ] ;
   then
     rm "$i" 2>/dev/null ;
   fi
 
-  MP4Box -add "$VFILE#trackID=1:fps=$FRAMERATE:name=" -packed \
-         -add "$AFILE#trackID=1:name=" -tmp . -new "$FILENAME.mp4" ;
+  MP4Box -add "$WORKINGDIR/$VFILE#trackID=1:fps=$FRAMERATE:name=" -packed \
+         -add "$WORKINGDIR/$AFILE#trackID=1:name=" -tmp "$WORKINGDIR" -new "$FILENAME.mp4" ;
   rc=$?
 
   if [ $rc -ne 0 ] ;
   then
     echo ERROR on MP4Box : "$i" ;
-    break
-  else
-    rm $VFILE $AFILE 2>/dev/null ;
+    exit 2
   fi
 done
 
-rm ./*isotmp 2>/dev/null ;
+rm "$WORKINGDIR"/$AFILE "$WORKINGDIR"/$VFILE 2>/dev/null ;
+exit 0
